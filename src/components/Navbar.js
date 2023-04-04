0382ba60cfc4a620ba528cd2d58c0e83823c2654
ndakalajulius <julius.ndakala@student.moringaschool>
2023-04-04 12:21:38 +0300

    added some stylings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavbarStyles.css";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 export const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <div className="header">
       <Link to="/">
         <h1> Portfolio</h1>
       </Link>
       <ul className="nav-menu">
-        <li> 
-        <Link to="/">Home</Link>
+        <li>
+          <Link to="/">Home</Link>
         </li>
-        <li> 
-        <Link to="/project">Project</Link>
+        <li>
+          <Link to="/project">Project</Link>
         </li>
-        <li> 
-        <Link to="/about">About</Link>
+        <li>
+          <Link to="/about">About</Link>
         </li>
-        <li> 
-        <Link to="/contact">Contact</Link>
+        <li>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "white" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "white" }} />
+        )}
+      </div>
     </div>
   );
 };
