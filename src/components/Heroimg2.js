ef57fc437f42176d30077ed943e52aba614ff4ab
ndakalajulius <julius.ndakala@student.moringaschool>
2023-04-05 14:03:32 +0300

    made changes to the hero components

---
+++ src/components/Heroimg2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HeroImg2Styles.css";
+const Heroimg2 = () => {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+      <h1> fghj</h1>
+      <p> vbnm </p>
+      </div>
+    </div>
+  );
+};
+
+export default Heroimg2;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import Heroimg2 from "../components/Heroimg2";
 const Project = () => {
   return (
     <div>
       <Navbar />
+      <Heroimg2/>
       <Footer />
     </div>
   );
