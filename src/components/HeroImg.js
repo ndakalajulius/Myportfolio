110e09e486f51f7ed493a6d6fd0f1eab41beb70b
ndakalajulius <julius.ndakala@student.moringaschool>
2023-04-05 11:20:28 +0300

    made changes in the hero

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import "./HeroImgStyles.css"
+import React from "react";
+import "./HeroImgStyles.css";
+import IntroImg from "../assets/intro-bg.jpg";
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
-    <div className='hero'>
-      <div className='mask'>
-      
+    <div className="hero">
+      <div className="mask">
+        <img className="into-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p> HI,I AM A FRELANCER</p>
+        <h1> React Developer </h1>
+        <div>
+          <Link to="/project" className="btn">
+            {" "}
+            projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            {" "}
+            Contact
+          </Link>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroImg
+export default HeroImg;
