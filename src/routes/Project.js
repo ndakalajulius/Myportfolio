fe88e2cecb01362ac6bc96ce34624316331d380e
ndakalajulius <julius.ndakala@student.moringaschool>
2023-04-06 15:23:58 +0300

    created work component

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import Heroimg2 from "../components/Heroimg2";
+import { PricingCard } from "../components/PricingCard";
+import WorkCard from "../components/WorkCard";
 const Project = () => {
   return (
     <div>
       <Navbar />
       <Heroimg2 heading= "PROJECTS." text="some of my most recent works "/>
+      <WorkCard/>
+      <PricingCard/>
       <Footer />
     </div>
   );
