b0cdf697a2aa81580607c39538d27c8d897df295
ndakalajulius <julius.ndakala@student.moringaschool>
2023-04-06 16:35:13 +0300

    made changes

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./WorkCardStyles.css";
 import pro1 from "../assets/project1.png";
 import { NavLink } from "react-router-dom";
+import WorkCardData from "./WorkCardData";
+import WorkCard from "./WorkCard";
 const Work = () => {
   return (
     <div className="work-container">
       <h1 className="project-heading"> Projects </h1>
-      <div className="project-container">
-        
-        
-      </div>
+      <div className="project-container"></div>
     </div>
   );
 };
