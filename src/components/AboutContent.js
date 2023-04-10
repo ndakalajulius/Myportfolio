42bf72f645f91447b598c0361626ace5d566a887
ndakalajulius <julius.ndakala@student.moringaschool>
2023-04-10 13:36:56 +0300

    styled the about content

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom"
 import "./AboutContentStyles.css"
 import React from 'react'
-
+import React1 from "../assets/react1.jpg"
+import React2 from "../assets/react2.webp"
 
 export const AboutContent = () => {
   return (
@@ -22,7 +23,15 @@ export const AboutContent = () => {
     </Link>
     </div>
     <div className="right">
+    <div className="img-container"> 
     
+    </div>
+    <div className="img-stack top">
+        <img src={React1} className="img" alt="true"/>
+    </div>
+    <div className="img-stack bottom">
+        <img src={React2} className="img" alt="true"/>
+    </div>
     </div>
     
     </div>
