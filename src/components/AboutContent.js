cea915e2ef6741d6f97f5cda16a9de3804cfe3b9
julius ndakala <juliusndakala35@gmail.com>
2023-05-24 21:50:55 +0300

    edited the about page

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom"
 import "./AboutContentStyles.css"
 import React from 'react'
-import React1 from "../assets/react1.jpg"
-import React2 from "../assets/react2.webp"
+
 
 export const AboutContent = () => {
   return (
     <div className="about">
     <div className="left">
     <h1> Who am I? </h1>
-    <p> I’ve developed numerous web applications. You name it, I’ve
+    <p>  I am a software developer.
+    I’ve developed numerous web applications. You name it, I’ve
     clocked an embarrassing number of hours online working on it. I’m
     confident my adaptability, web developing experience and
     commitment would make me a perfect fit for the work.
@@ -22,17 +22,12 @@ export const AboutContent = () => {
      <button className="btn">contact </button>
     </Link>
     </div>
-    <div className="right">
-    <div className="img-container"> 
     
-    </div>
-    <div className="img-stack top">
-        <img src={React1} className="img" alt="true"/>
-    </div>
-    <div className="img-stack bottom">
-        <img src={React2} className="img" alt="true"/>
-    </div>
-    </div>
+    
+    
+    
+    
+    
     
     </div>
   )
