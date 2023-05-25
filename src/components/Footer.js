c0a2fd7c633290a9848dc967330ec3129b82673c
julius ndakala <juliusndakala35@gmail.com>
2023-05-25 12:36:39 +0300

    made changes i nfooter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,65 +1,66 @@
 import React from "react";
 import "./FooterStyles.css";
-import { FaFacebook, FaHome, FaLinkedin, FaMailBulk, FaPhone, FaTwitter } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faMedium,
+  faStackOverflow,
+  faGithub,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+   
+  
+
+} from "@fortawesome/free-brands-svg-icons";
+
 export const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-container">
         <div className="left">
           <div className="location">
-            <FaHome size={20} style={{ color: "white", marginRight: "2rem" }} />
-            <div>
-              <p> Dhanjay Apartments, Nairobi</p>
-              <p> Kenya </p>
-            </div>
-          </div>
-          <div className="phone">
-            <h4>
-              {" "}
-              <FaPhone
-                size={20}
-                style={{ color: "white", marginRight: "2rem" }}
-              />
-              +254700308204
-            </h4>
-          </div>
-          <div className="email">
-            <h4>
-              {" "}
-              <FaMailBulk 
-                size={20}
-                style={{ color: "white", marginRight: "2rem" }}
-              />
-              juliusndakala35@gmail.com
-            </h4>
-          </div>
+          <h4> About me </h4>
+          <p> I am a freelancer, I develop web applications.
+          I’m fiercely competitive in my approach, up-to-date on all the latest
+          trends.</p>
         </div>
         <div className="right"> 
-        <h4> About me </h4>
-        <p> I am a freelancer, I develop web applications.
-        I’m fiercely competitive in my approach, up-to-date on all the latest
-        trends.</p>
+        
         <div className="social ">
-        <FaFacebook 
-        size={30}
-        style={{ color: "white", marginRight: "1rem" }}
-        
-      />
-     
-      <FaTwitter
-        size={30}
-        style={{ color: "white", marginRight: "1rem" }}
-        
-      />
-      <FaLinkedin
-        size={30}
-        style={{ color: "white", marginRight: "1rem" }}
-        
-      />
+
+        <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a href="https://www.instagram.com/learnbuildteach"
+        className="instagram social">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>    
+      <a href="https://www.facebook.com/learnbuildteach/"
+      className="facebook social">
+      <FontAwesomeIcon icon={faFacebook} size="2x" />
+    </a>
+    <a href="https://www.facebook.com/learnbuildteach/"
+    className="facebook social">
+    <FontAwesomeIcon icon={faGithub} size="2x" />
+  </a>
+  <a href="https://www.facebook.com/learnbuildteach/"
+  className="facebook social">
+  <FontAwesomeIcon icon={faMedium} size="2x" />
+</a>
+<a href="https://www.facebook.com/learnbuildteach/"
+className="facebook social">
+<FontAwesomeIcon icon={faLinkedin} size="2x" />
+</a>
+<a href="https://stackoverflow.com/users/21133024/julius-ndakala"
+className="stackoverflow social">
+<FontAwesomeIcon icon={faStackOverflow} size="2x" />
+</a>
 
         </div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
