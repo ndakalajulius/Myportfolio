ace87bbfee56bb9bd81ff495fe01fa16ea00195a
julius ndakala <juliusndakala35@gmail.com>
2023-05-29 19:56:28 +0300

    made changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,63 +1,74 @@
 import React from "react";
 import "./FooterStyles.css";
-import { FaFacebook, FaHome, FaLinkedin, FaMailBulk, FaPhone, FaTwitter } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  
+  faLinkedinIn,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faHome ,faPhone, } from "@fortawesome/free-solid-svg-icons";
+
 export const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-container">
         <div className="left">
           <div className="location">
-            <FaHome size={20} style={{ color: "white", marginRight: "2rem" }} />
+            <a href="http://localhost:3000/" className="home social">
+              <FontAwesomeIcon icon={faHome} size="2x" />
+            </a>
             <div>
               <p> Dhanjay Apartments, Nairobi</p>
               <p> Kenya </p>
             </div>
           </div>
           <div className="phone">
-            <h4>
-              {" "}
-              <FaPhone
-                size={20}
-                style={{ color: "white", marginRight: "2rem" }}
-              />
-              +254700308204
-            </h4>
+            <a
+              href="+254 700-308-204"
+              className="phone social"
+            >
+              <FontAwesomeIcon icon={faPhone} size="2x" />
+            </a>
           </div>
           <div className="email">
-            <h4>
-              {" "}
-              <FaMailBulk 
-                size={20}
-                style={{ color: "white", marginRight: "2rem" }}
-              />
-              juliusndakala35@gmail.com
-            </h4>
+            <a
+              href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
+              className="email social"
+            >
+              <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            </a>
           </div>
         </div>
-        <div className="right"> 
-        <h4> About me </h4>
-        <p> I am a freelancer, I develop web applications.
-        I’m fiercely competitive in my approach, up-to-date on all the latest
-        trends.</p>
-        <div className="social ">
-        <FaFacebook 
-        size={30}
-        style={{ color: "white", marginRight: "1rem" }}
-        
-      />
-     
-      <FaTwitter
-        size={30}
-        style={{ color: "white", marginRight: "1rem" }}
-        
-      />
-      <FaLinkedin
-        size={30}
-        style={{ color: "white", marginRight: "1rem" }}
-        
-      />
+        <div className="right">
+          <h4> About me </h4>
+          <p>
+            {" "}
+            I am a freelancer, I develop web applications. I’m fiercely
+            competitive in my approach, up-to-date on all the latest trends.
+          </p>
+          <div className="social ">
+            <a
+              href="https://github.com/ndakalajulius"
+              className="github social"
+            >
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
 
-        </div>
+            <a
+              href="https://www.twitter.com/ndakalawa"
+              className="twitter social"
+            >
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/juliusndakala/"
+              className="linkedin social"
+            >
+              <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
