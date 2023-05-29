61faa6a3226fd9ad64e85d56f21b6bcd412efc97
julius ndakala <juliusndakala35@gmail.com>
2023-05-29 19:04:35 +0300

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,78 +1,71 @@
 import React from "react";
-import "./FooterStyles.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container, Row, Col } from "react-bootstrap";
 import {
-  faLinkedin,
-  faMedium,
-  faStackOverflow,
-  faGithub,
-  faFacebook,
-  faTwitter,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
-export const Footer = () => {
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
   return (
-    <div className="footer">
-      <div className="footer-container">
-        <div className="left">
-          <div className="location">
-            <h4> About me </h4>
-            <p>
-              {" "}
-              I am a freelancer, I develop web applications. I’m fiercely
-              competitive in my approach, up-to-date on all the latest trends.
-            </p>
-          </div>
-          <div className="right">
-            <div className="social-container ">
-            <h1> Follow me </h1>
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Soumyajit Behera</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} SB</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
               <a
-                href="https://twitter.com/NdakalaWa"
-                className="twitter social"
+                href="https://github.com/soumyajit4419"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
               >
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
+                <AiFillGithub />
               </a>
+            </li>
+            <li className="social-icons">
               <a
-                href="https://www.instagram.com/juliusndakala/"
-                className="instagram social"
+                href="https://twitter.com/Soumyajit4419"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
               >
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
+                <AiOutlineTwitter />
               </a>
+            </li>
+            <li className="social-icons">
               <a
-                href="https://www.facebook.com/julius.ndakala/"
-                className="facebook social"
+                href="https://www.linkedin.com/in/soumyajit4419/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
               >
-                <FontAwesomeIcon icon={faFacebook} size="2x" />
+                <FaLinkedinIn />
               </a>
+            </li>
+            <li className="social-icons">
               <a
-                href="https://github.com/ndakalajulius"
-                className="github social"
+                href="https://www.instagram.com/soumyajit4419"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
               >
-                <FontAwesomeIcon icon={faGithub} size="2x" />
+                <AiFillInstagram />
               </a>
-              <a
-                href="https://medium.com/@juliusndakala35"
-                className="medium social"
-              >
-                <FontAwesomeIcon icon={faMedium} size="2x" />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/juliusndakala/"
-                className="linkeidin social"
-              >
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
-              </a>
-              <a
-                href="https://stackoverflow.com/users/21133024/julius-ndakala"
-                className="stackoverflow social"
-              >
-                <FontAwesomeIcon icon={faStackOverflow} size="2x" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
   );
-};
+}
+
+export default Footer;
