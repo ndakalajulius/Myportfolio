8d9f9ad1e335cda544c948e80089bd03c4ea85d3
julius ndakala <juliusndakala35@gmail.com>
2023-09-27 15:50:18 +0300

    made some changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,54 +1,49 @@
 import React from "react";
-import SimpleReactFooter from 'simple-react-footer';
-
+import "./Footer.css";
 const Footer = () => {
-  // Define the data for the footer
-  const description =
-    "I am a software developer I’ve developed numerous web applications. You name it, I’ve clocked an embarrassing number of hours online working on it. I’m confident my adaptability, web developing experience and commitment would make me a perfect fit for the work.Currently, I am a freelancer, I develop web applications. I’m fiercely competitive in my approach, up-to-date on all the lates trends.I’m positive my knack for developing web applications will makeme an immediate asset";
-  const title = " More about me";
-
-  const columns = [
-    {
-      title: "Links",
-      resources: [
-        {
-          name: "Contact",
-          link: "/contact",
-        },
-        {
-          name: "About ",
-          link: "/about",
-        },
-        {
-          name: "Home",
-          link: "/home",
-        },
-        {
-          name: " Projects",
-          link: "/project",
-        },
-      ],
-    },
-   
-  ];
-
   return (
-    <SimpleReactFooter
-      description={description}
-      title={title}
-      columns={columns}
-      linkedin="https://www.linkedin.com/in/julius-ndakala-33ba171a5/"
-      facebook="https://www.facebook.com/"
-      twitter="https://twitter.com/NdakalaWar"
-      instagram="https://www.instagram.com/juliusndakala/"
-      youtube="UCFt6TSF464J8K82xeA?"
-      pinterest="https://www.pinterest.com/jndakala/"
-      copyright="black"
-      iconColor="black"
-      backgroundColor="grey"
-      fontColor="black"
-      copyrightColor="black"
-    />
+    <div class="footer-content">
+      <div class="footer-logo">
+        <p>
+          Software engineering graduate looking for real-world experience in
+          this field. Interested in learning more about the development of
+          mobile applications, web applications and accessible software. Has
+          experience implementing computer systems in low-income communities,
+          helping a new generation of students become digitally literate. Has
+          proficiency in both front-end and back-end software development.
+        </p>
+      </div>
+      <div class="footer-links">
+        <ul>
+          <li>
+            <a href="./home">Home</a>
+          </li>
+          <li>
+            <a href="./about">About</a>
+          </li>
+          <li>
+            <a href="./projects">projects</a>
+          </li>
+          <li>
+            <a href="./contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="footer-social">
+        <ul>
+          <li>
+            <a href="https://www.facebook.com/julius.ndakala/"></a>
+          </li>
+          <li>
+            <a href="https://twitter.com/ndakala_julius"></a>
+          </li>
+          <li>
+            <a href="linkedin.com/in/julius-ndakala-33ba171a5"></a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
